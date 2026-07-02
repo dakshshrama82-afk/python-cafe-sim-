0999e4fc1c19648082c44f833fffa78d750c407d
Daksh Sharma <dakshshrama82@gmail.com>
2026-07-02 23:55:22 +0530

    refine the code it is still lest with 1 error

--- python-cafe-sim.py
+++ python-cafe-sim.py
@@ -15,20 +15,46 @@ menu = {
 }
 print( menu )
 
+#ordering the item from the menu and checking if the item is in the menu or not
+
 order = input("please enter the item you would like to order from the menu:")
-if order != menu:
+if order in menu:
+    quantity = int(input("how many would you like to order"))
+else:
     print("we do not have that item in our menu. Please choose an item from the menu.")
+    order = input("please enter the item you would like to order from the menu:")
+    quantity = int(input("how many would you like to order"))
 
 add_item = input("would you like to add any other item to your order? (yes/no):")
-if add_item== "yes":
+if add_item == "yes":
     order2 = input("please enter the item you would like to order from the menu:")
-else:
+    if order2 not in menu:
+        print("we do not have that item in our menu. Please choose an item from the menu.")
+        order2 = input("please enter the item you would like to order from the menu:")
+    quantity2 = int(input("how many would you like to order"))
+if add_item == "no":
     print("Thank you for your order. We will prepare it for you.")
 
-  
+#taking the quantity of the order and calculating the total bill
+if quantity2 > 0:
+    totalquantity = quantity + quantity2
+else:
+    totalquantity = quantity
 
-quantity = int(input("how manny would you like to order"))
-prise = menu[order] + menu[order2]
-sum = prise* quantity
+if order2 in menu:
+    totalprise = menu[order] + menu[order2]
+else:
+    totalprise = menu[order]
+sum = totalprise * totalquantity
 
 print("Thank you for your order, ", Titel, clinte_name, ". Your total bill is: Rs.", sum)
+
+#printing the receipt for the order
+
+print("--------cafe receipt--------")
+print("Customer Name:", Titel, clinte_name)
+print("Order:", order, "and", order2)
+print("Quantity:", totalquantity)
+print("Total Bill: Rs.", sum)
+
+print("We hope you enjoy your meal! Have a great day!")
